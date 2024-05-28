02c59912876e68dd2b89cda157f8e5fff3d285c6
Vladyslav Dmytrenko <v.dmitrenko1998@gmail.com>
2024-05-28 18:23:03 +0300

    rewrote aside matches api calls

--- components/matches/asideMatches.tsx
+++ components/matches/asideMatches.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import MatchItem from "./matchItem";
-import { fetchData } from "@/utils/fetchData";
 import { useEffect, useState } from "react";
 import { API } from "@/services/api";
-import { IHeroes } from "@/services/api/endpoints/types";
+import { IHeroes, IPublicMatches } from "@/services/api/endpoints/types";
 
 export type TMatch = {
   match_id: number;
@@ -18,37 +16,32 @@ export type TMatch = {
 // const getTeamHeroes = (heroes, matches) => {};
 
 const AsideMatches = () => {
-  const [heroes, setHeroes] = useState([]);
-  const [matches, setMatches] = useState([]);
-
-  useEffect(() => {
-    API.heroes.getHeroes().then((data) => {
-      console.log(data);
-      setHeroes(data);
-    });
-  }, []);
+  const [matches, setMatches] = useState<any | null>(null);
+  const [matchesLoading, setMatchesLoading] = useState(true);
 
   useEffect(() => {
     API.matches
-      .getPublicMatches(
-        "https://api.opendota.com/api/publicMatches?min_rank=80"
-      )
-      .then((data) => setMatches(data));
+      .getPublicMatches("?min_rank=80")
+      .then((data) => setMatches(data))
+      .catch((error) => {})
+      .finally(() => {
+        setMatchesLoading(false);
+      });
   }, []);
 
   return (
     <aside className="w-1/3">
       <h2 className="text-left text-white mb-1 text-xl">Latest Pro Publics</h2>
       <div className="flex flex-col gap-3">
-        {matches.map((match: TMatch, index: number) => {
-          if (index <= 8) {
-            return (
-              <MatchItem key={match.match_id} match={match} heroes={heroes} />
-            );
-          } else {
-            return;
-          }
-        })}
+        {matchesLoading && <div>Loading...</div>}
+        {!matchesLoading &&
+          matches?.map((match: TMatch, index: number) => {
+            if (index <= 8) {
+              return <MatchItem key={match.match_id} match={match} />;
+            } else {
+              return;
+            }
+          })}
       </div>
     </aside>
   );
--- components/matches/matchItem.tsx
+++ components/matches/matchItem.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import HeroIcon from "./heroIcon";
 import { TMatch } from "./asideMatches";
 import { THero } from "@/app/heroes/page";
+import { IHeroes } from "@/services/api/endpoints/types";
+import { useEffect, useState } from "react";
+import { API } from "@/services/api";
 
-const getMatchHeroes = (heroes: THero[], arr: number[]) => {
+const getMatchHeroes = (heroes: IHeroes, arr: number[]) => {
   const heroesArray = arr?.map((id) => {
     return heroes.find((hero: any) => hero.id === id);
   });
@@ -24,49 +27,68 @@ const convertTime = (time: number) => {
   };
 };
 
-const MatchItem = ({ match, heroes }: { match: TMatch; heroes: THero[] }) => {
+const MatchItem = ({ match }: { match: TMatch }) => {
+  const [heroes, setHeroes] = useState<IHeroes>([]);
+  const [heroesLoading, setHeroesLoading] = useState(true);
+
+  useEffect(() => {
+    API.heroes
+      .getHeroes()
+      .then((data) => {
+        setHeroes(data);
+      })
+      .catch((error) => {})
+      .finally(() => {
+        setHeroesLoading(false);
+      });
+  }, []);
+
   const radiantHeroes = getMatchHeroes(heroes, match.radiant_team);
   const direHeroes = getMatchHeroes(heroes, match.dire_team);
   const matchDuration = convertTime(match.duration);
 
   return (
-    <div className="p-2 bg-neutral-500/20 text-white">
-      <div className="flex items-center gap-2">
-        <h3 className="text-xl">Match {match.match_id}</h3>
-        <Image
-          src={"/ranks/immortal.png"}
-          width={40}
-          height={40}
-          alt="Immortal Rank"
-        />
-      </div>
-      <div
-        className={`${
-          match.radiant_win ? "text-green-500" : "text-red-500"
-        } text-center uppercase`}
-      >
-        Winner {match.radiant_win ? "Radiant" : "Dire"}
-      </div>
-      <div className="text-center">
-        {matchDuration.minutes}:{matchDuration.seconds}
-      </div>
-      <div>
-        <p className="mb-2 text-green-500 text-left">Radian</p>
-        <div className="flex gap-2">
-          {radiantHeroes.map((hero: any) => (
-            <HeroIcon key={hero.id} name={hero.localized_name} />
-          ))}
+    <>
+      {!heroesLoading && (
+        <div className="p-2 bg-neutral-500/20 text-white transition-all">
+          <div className="flex items-center gap-2">
+            <h3 className="text-xl">Match {match.match_id}</h3>
+            <Image
+              src={"/ranks/immortal.png"}
+              width={40}
+              height={40}
+              alt="Immortal Rank"
+            />
+          </div>
+          <div
+            className={`${
+              match.radiant_win ? "text-green-500" : "text-red-500"
+            } text-center uppercase`}
+          >
+            Winner {match.radiant_win ? "Radiant" : "Dire"}
+          </div>
+          <div className="text-center">
+            {matchDuration.minutes}:{matchDuration.seconds}
+          </div>
+          <div>
+            <p className="mb-2 text-green-500 text-left">Radian</p>
+            <div className="flex gap-2">
+              {radiantHeroes.map((hero: any) => (
+                <HeroIcon key={hero.id} name={hero.localized_name} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <p className="mt-4 mb-2 text-red-500 text-left">Dire</p>
+            <div className="flex gap-2">
+              {direHeroes.map((hero: any) => (
+                <HeroIcon key={hero.id} name={hero.localized_name} />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <p className="mt-4 mb-2 text-red-500 text-left">Dire</p>
-        <div className="flex gap-2">
-          {direHeroes.map((hero: any) => (
-            <HeroIcon key={hero.id} name={hero.localized_name} />
-          ))}
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
