a878c3f310870dc901535d40f27b4911665f7499
Vladyslav Dmytrenko <v.dmitrenko1998@gmail.com>
2024-06-20 20:53:09 +0300

    added pagination for heroPlayers component

--- components/hero/players/heroPlayers.tsx
+++ components/hero/players/heroPlayers.tsx
@@ -1,24 +1,49 @@
+import {useMemo, useState} from "react";
+
 import {IHeroPlayer} from "@/services/api/endpoints/types";
 import HeroPlayer from "@components/hero/players/heroPlayer";
+import Pagination from "@components/ui/pagination/pagination";
 
 type THeroPlayersProps = {
   heroPlayers: IHeroPlayer[];
 }
 
 const HeroPlayers = ({heroPlayers}: THeroPlayersProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, SetPostsPerPage] = useState(100);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * postsPerPage;
+    const lastPageIndex = firstPageIndex + postsPerPage;
+    return heroPlayers.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  };
+
   return (
     <>
-      <h3 className="mb-3 text-[#ffffff99] text-xls"><span className="font-medium text-[#ffffffde]">
+      <div className="mb-2 flex items-center justify-between">
+        <h3 className="text-[#ffffff99] text-xls"><span className="font-medium text-[#ffffffde]">
         Players</span> Data from professional matches
-      </h3>
-      <article className="rounded border-amber-50/50 border border-gray-300/10">
+        </h3>
+        <Pagination
+          siblingCount={1}
+          currentPage={currentPage}
+          totalCount={heroPlayers.length}
+          pageSize={postsPerPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+      <article className="rounded border border-gray-300/10">
         <header className="flex px-6 py-2 rounded-t text-white uppercase bg-gradient-to-r from-[#020024] to-[#2da65c]">
           <div className="w-6/12 text-base">ACCOUNT ID</div>
           <div className="w-2/12 text-base text-center cursor-pointer">GAMES</div>
           <div className="w-2/12 text-base text-center cursor-pointer">WIN %</div>
           <div className="w-2/12 text-base text-center cursor-pointer">ADVANTAGE</div>
         </header>
-        {heroPlayers && heroPlayers.map((player) =>
+        {heroPlayers && currentTableData.map((player) =>
           <HeroPlayer
             key={player.account_id} id={player.account_id}
             games={player.games_played} wins={player.wins}
---
+++ components/ui/pagination/pagination.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {usePagination, DOTS} from "@/common/hooks/usePagination";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+
+type TPaginationProps = {
+  onPageChange: (page: number) => void;
+  totalCount: number;
+  siblingCount: number;
+  currentPage: number;
+  pageSize: number;
+}
+
+const Pagination = (props: TPaginationProps) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize
+  });
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange[paginationRange.length - 1];
+
+  return (
+    <div className="mb-2">
+      {/* Left navigation arrow */}
+      <button className="px-3"
+              disabled={currentPage === 1}
+              onClick={onPrevious}
+      >
+        <ArrowCircleLeftIcon style={{fill: `${currentPage === 1 ? 'grey' : ""}`}}/>
+      </button>
+
+      {paginationRange?.map((pageNumber, index) => {
+        // If the pageItem is a DOT, render the DOTS unicode character
+        if (pageNumber === DOTS) {
+          return <button key={index} className="px-2 py-1">&#8230;</button>;
+        }
+
+        // Render our Page Pills
+        return (
+          <button
+            key={index}
+            className={pageNumber === currentPage ? "px-3 py-1 bg-[#2da65c] rounded " : "px-2 py-1"}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        );
+      })}
+      {/*  Right Navigation arrow */}
+      <button
+        disabled={currentPage === lastPage}
+        onClick={onNext}
+      >
+        <div className={`px-3`}><ArrowCircleRightIcon style={{fill: `${currentPage === lastPage ? 'grey' : ""}`}}/>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
