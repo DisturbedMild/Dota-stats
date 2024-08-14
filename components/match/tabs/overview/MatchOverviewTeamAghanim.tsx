04bf84bd385d621537fc475d77c2cb862d3dffeb
DisturbedMild <vladyslav1@4937.local>
2024-08-14 21:13:55 +0300

    Added tooltip for aghanim and shard on match page

--- components/match/tabs/overview/MatchOverviewTeamAghanim.tsx
+++ components/match/tabs/overview/MatchOverviewTeamAghanim.tsx
@@ -1,8 +1,11 @@
 import {Tooltip} from "react-tooltip";
+import abilitiesData from "dotaconstants/build/abilities.json"
 import aghanimsDescriptionsData from 'dotaconstants/build/aghs_desc.json'
 import Image from "next/image";
 
-import {AghDescription} from "@/common/types";
+import {Ability, AghDescription} from "@/common/types";
+import AghanimPopup from "@/components/hero/abilities/AghanimPopup";
+import AghanimShardPopup from "@/components/hero/abilities/AghanimShardPopup";
 
 interface MatchOverviewTeamAghanimProps {
   id: number,
@@ -10,58 +13,77 @@ interface MatchOverviewTeamAghanimProps {
   haveShard: number
 }
 
-// const getHeroAghanimAndShard = (id: number, aghsWithDesc: AghDescription[]): AghDescription => {
-//   return aghsWithDesc.find((item) => item.hero_id === id)
-// }
+const getHeroAghanimAndShard = (id: number, aghsWithDesc: AghDescription[]): AghDescription | null => {
+  const aghanimDesc = aghsWithDesc.find((item) => item.hero_id === id)
+   if(aghanimDesc) {
+     return aghanimDesc
+   }
+  return null
+}
+
+const getAbility = (name: string, abilities: Record<string, Ability>): Ability | undefined => {
+  for (const key in abilities) {
+    if (abilities[key].dname === name) {
+      return abilities[key]
+    }
+  }
+}
 
 const MatchOverviewTeamAghanim = ({id, haveAghanim, haveShard}: MatchOverviewTeamAghanimProps) => {
   const aghanimsDescriptions = JSON.parse(JSON.stringify(aghanimsDescriptionsData));
-  // const currentHeroAghanimDescription: AghDescription = getHeroAghanimAndShard(id, aghanimsDescriptions)
+  const abilities: Record<string, Ability> = JSON.parse(JSON.stringify(abilitiesData));
+  const currentHeroAghanimDescription = getHeroAghanimAndShard(id, aghanimsDescriptions);
+  const getAghanimAbilityWithDescription = currentHeroAghanimDescription ? getAbility(currentHeroAghanimDescription.scepter_skill_name, abilities) : null;
+  const getShardAbilityWithDescription = currentHeroAghanimDescription ? getAbility(currentHeroAghanimDescription.shard_skill_name, abilities) : null;
+
+
   return <td className="text-center h-20 w-[30px] text-xls">
     <div className="flex flex-col items-center">
-      <div>
+      <div className={`scepter-anchor-${id}`}>
         {haveAghanim === 0 ? (
           <Image
             src={"/icons/scepter.png"}
             width={24}
             height={24}
             alt="Aghanim scepter"
-            className="cursor-pointer scepter-anchor"
+            className="cursor-pointer"
           />) : (
           <Image
             src={"/icons/scepter_active.png"}
             width={24}
             height={24}
             alt="Aghanim scepter"
-            className="cursor-pointer scepter-anchor"
+            className="cursor-pointer"
           />
         )
         }
-        <Tooltip anchorSelect=".scepter-anchor" place="left">
-          Scepter description
+        <Tooltip anchorSelect={`.scepter-anchor-${id}`} place="left"
+                 style={{padding: 0, zIndex: 1000, textAlign: "left", opacity: 0.9}}>
+          {getAghanimAbilityWithDescription ? <AghanimPopup aghanimAbility={getAghanimAbilityWithDescription}/> : null}
         </Tooltip>
       </div>
-      <div>
+      <div className={`shard-anchor-${id}`}>
         {haveShard === 0 ? (
           <Image
             src={"/icons/shard.png"}
             width={24}
-            height={16}
-            alt="Aghanim's shard"
-            className="cursor-pointer shard-anchor"
-          />
-        ) : (
+            height={24}
+            alt="Aghanim scepter"
+            className="cursor-pointer"
+          />) : (
           <Image
             src={"/icons/shard_active.png"}
             width={24}
-            height={16}
-            alt="Aghanim's shard"
-            className="cursor-pointer shard-anchor"
+            height={24}
+            alt="Aghanim scepter"
+            className="cursor-pointer"
           />
         )
         }
-        <Tooltip anchorSelect=".shard-anchor" place="left">
-          Shard description
+        <Tooltip anchorSelect={`.shard-anchor-${id}`} place="left"
+                 style={{padding: 0, zIndex: 1000, textAlign: "left", opacity: 0.9}}>
+          {getShardAbilityWithDescription ?
+            <AghanimShardPopup aghanimShardAbility={getShardAbilityWithDescription}/> : null}
         </Tooltip>
       </div>
     </div>
